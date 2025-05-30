c615950935fa4db9625f711525106ebeb7eba0a7
Hajar Ouakim <hajar.ouakim1@gmail.com>
2025-05-30 11:20:48 +0100

    print tests for zapier

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ app = Flask(__name__)
 @app.route("/webhook", methods=["POST"])
 def my_function():
     data = request.json
+    print("📩 Requête reçue:", data)
     mail = data.get("user_mail", "")
     answer = my_gpt(mail)
+    print("Réponse GPT:", answer)
 
     return jsonify({"answer": answer})
 
