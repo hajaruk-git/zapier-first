76acd2bfcf10e713dba217139fdf831dd058dbb0
Hajar Ouakim <hajar.ouakim1@gmail.com>
2025-05-30 11:01:50 +0100

    Fix port for Render

--- app.py
+++ app.py
@@ -11,4 +11,5 @@ def my_function():
     return jsonify({"answer": answer})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
